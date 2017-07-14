53b8bcb221cb6e0e85aa71039f095eb49e27a562
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-07-14 10:13:50 -0700

    created array from input from 1 to number specified by user.

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,14 @@ var returnArr = [];
 // Count up to 2 from 1 when user inputs 2
 
 var pingPong = function(number) {
+  debugger;
   var initalValue = 1;
-  if (initalValue <= number) {
+  if (initalValue < number) {
     for (i = 1; i <= number; i++) {
     returnArr.push(i);
     initalValue += 1;
     }
+    console.log(returnArr)
   }
   }
 
