167051506b4091e852be4b40d094f9e270b45ab2
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-07-14 09:34:13 -0700

    add initial logic to complete spec 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 // Business Logic -->
 
+var pingPong = function(number) {
+  
+}
+
 
 
 // UI Logic -->
 $(function(){
-  $("number-input").submit(function(event){
+  $("#number-input").submit(function(event){
     event.preventDefault();
-    var origNumber = $("number").val();
-    $("")
+    var origNumber = $("#number").val();
+    $("#output ul").append("<li>" + origNumber + "</li");
   });
 });
