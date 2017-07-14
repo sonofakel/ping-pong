78ec9c23582067e9f1f9d3a51bebded288bd7116
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-07-14 10:54:12 -0700

    add logic to display 1 & 2 as list items on page, spec 1.

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,23 @@ var returnArr = [];
 // Count up to 2 from 1 when user inputs 2
 
 var pingPong = function(number) {
+  debugger;
   var initalValue = 1;
   if (initalValue < number) {
     for (i = 1; i <= number; i++) {
-    returnArr.push("<li>" + i + "</li>");
+    returnArr.push(i);
     initalValue += 1;
     }
+  }
+  for (i = 0; i < returnArr; i++) {
+     
+  }
 
-    console.log(returnArr.join());
-  }
-  for (i = 0; 1 < returnArr.length; i++) {
-    
-  }
+
+  // for (i = 0; 1 < returnArr.length; i++) {
+  //
+  //
+  // }
   }
 
 
@@ -27,7 +32,7 @@ var pingPong = function(number) {
 $(function(){
   $("#number-input").submit(function(event){
     event.preventDefault();
-    var number = $("#number").val();
-    $("#output ul").append(pingPong(number));
+    var Orignumber = $("#number").val();
+    $("#output ul").text(pingPong(Orignumber));
   });
 });
