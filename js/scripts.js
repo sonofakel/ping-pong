08f077a2b3eb114840fab5821d8ea1474344aa19
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-07-14 13:01:27 -0700

    specs 3-9 complete.

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,24 @@ var pingPong = function(number) {
   var initalValue = 1;
   if (initalValue < number) {
     for (i = 1; i <= number; i++) {
-      returnArr.push("<li>" + i + "</li>");
+      returnArr.push(i);
       initalValue += 1;
     }
   }
+  for (i = 0; i < returnArr.length; i++) {
+  debugger;
+    if (returnArr[i] % 15 === 0) {
+       returnArr[i] = "pingpong";
+    } else if (returnArr[i] % 3 === 0 ) {
+      returnArr[i] = "ping";
+    } else if (returnArr[i] % 5 === 0) {
+      returnArr[i] = "pong";
+    }
+
+  }
+
+
+
     var join = returnArr.join(" ");
     return join;
 }
